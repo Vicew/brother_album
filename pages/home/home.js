7bacdf4839e7a18053bf121450acc862c2c42cfa
pandaomeng <806636588@qq.com>
2019-03-10 16:03:09 +0800

    home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,18 @@
-// pages/home/home.js
+import regeneratorRuntime from '../../libs/regenerator-runtime.js'
+import interaction from '../../utils/interaction.js';
+import login from '../../utils/login.js'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.goPage()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async goPage() {
+    await login()
   }
 })
