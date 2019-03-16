1099814c38ae268cce9cf447c87feb698e91273e
Panxiandiao <1045366459@qq.com>
2019-03-16 20:19:29 +0800

    图片预览及保存问题

--- pages/home/home.js
+++ pages/home/home.js
@@ -16,6 +16,13 @@ Page({
     this.goPage()
   },
 
+  showBigImg:function(option){
+    wx.previewImage({
+      current: option.target.dataset.src, // 当前显示图片的http链接
+      urls: [this.data.pictureList] // 需要预览的图片http链接列表
+    })
+  },
+
   async goPage() {
     await login()
     const res = await axios.get('/picture')
